58a23c571f93a547614c8d11dff0ed93c00ef672
Dracorike <pedrohenriqueknd@gmail.com>
2023-01-02 15:23:03 -0300

    [main] Reforma no chart_bar.

--- lib/ui/components/chart_bar.dart
+++ lib/ui/components/chart_bar.dart
@@ -13,48 +13,51 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 18,
-          child: FittedBox(
-            child: Text(value.toStringAsFixed(2)),
+    return LayoutBuilder(builder: (context, constraint) {
+      return Column(
+        children: [
+          SizedBox(
+            height: constraint.maxHeight * 0.1,
+            child: FittedBox(
+              child: Text(value.toStringAsFixed(2)),
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 5,
-        ),
-        SizedBox(
-          width: 10,
-          height: 60,
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1.0,
-                  ),
-                  color: const Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-              ),
-              FractionallySizedBox(
-                alignment: AlignmentDirectional.bottomCenter,
-                heightFactor: percentage,
-                child: Container(
+          SizedBox(height: constraint.maxHeight * 0.05),
+          SizedBox(
+            width: 10,
+            height: constraint.maxHeight * 0.6,
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                    color: const Color.fromRGBO(220, 220, 220, 1),
                     borderRadius: BorderRadius.circular(5),
                   ),
                 ),
-              )
-            ],
+                FractionallySizedBox(
+                  alignment: AlignmentDirectional.bottomCenter,
+                  heightFactor: percentage,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-        Text(label),
-      ],
-    );
+          SizedBox(
+            height: constraint.maxHeight * 0.05,
+          ),
+          Text(label),
+        ],
+      );
+    });
   }
 }
