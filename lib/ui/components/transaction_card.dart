cdbae8e7a615e0f5ba1f6e78811c053bd4ef219a
Dracorike <pedrohenriqueknd@gmail.com>
2023-01-02 16:22:53 -0300

    [clean_code] Cleaning TransactionCard component.

--- lib/ui/components/transaction_card.dart
+++ lib/ui/components/transaction_card.dart
@@ -1,22 +1,11 @@
 import 'package:financas/model/transaction.dart';
+import 'package:financas/util/format_string.dart';
 import 'package:flutter/material.dart';
 
 class TransactionCard extends StatelessWidget {
   const TransactionCard({super.key, required this.transaction});
   final Transaction transaction;
   final double sizeBox = 50;
-
-  String formatDateString(String date) {
-    if (date.isEmpty) return '10/10/2010';
-    var splitedString = date.substring(0, 10).split('-').reversed;
-    var formatDateString = splitedString
-        .toString()
-        .replaceAll('(', '')
-        .replaceAll(')', '')
-        .replaceAll(', ', '/');
-
-    return formatDateString;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +15,11 @@ class TransactionCard extends StatelessWidget {
           Container(
             margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
             decoration: BoxDecoration(
-                border: Border.all(color: Colors.black, width: 2)),
+              border: Border.all(
+                color: Colors.black,
+                width: 2,
+              ),
+            ),
             padding: const EdgeInsets.all(10),
             child: Text(
               'R\$ ${transaction.value.toStringAsFixed(2).replaceAll('.', ',')} ',
@@ -43,7 +36,7 @@ class TransactionCard extends StatelessWidget {
                   style: const TextStyle(
                       fontSize: 16, fontWeight: FontWeight.bold)),
               Text(
-                formatDateString(transaction.date.toString()),
+                FormatString.formatDateString(transaction.date.toString()),
                 style: const TextStyle(color: Colors.grey),
               )
             ],
